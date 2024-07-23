360f28d024855872b6fe644744836b9573c73cfd
Theo Madzou <madztheo@hotmail.fr>
2024-07-23 15:38:05 +0200

    initial commit

---
+++ lib/build.gradle.kts
@@ -0,0 +1,129 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("de.undercouch.download") version "5.6.0"
+}
+
+android {
+    namespace = "com.noirandroid.lib"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.madztheo"
+                artifactId = "noir_android"
+                version = "0.30.0-1"
+            }
+        }
+    }
+}
+
+val rustLibName = "noir_java" // Adjust based on your library name
+val rustLibPath = "src/main/java/$rustLibName" // Adjust based on your library name
+
+tasks.register<Exec>("buildRust") {
+    workingDir(file(rustLibPath))
+    // Set the environment variables necessary for OpenSSL
+    val androidHome = System.getenv("ANDROID_HOME")
+    val ndkVersion = System.getenv("NDK_VERSION")
+    val hostTag = System.getenv("HOST_TAG")
+    val path = System.getenv("PATH")
+    val androidNdkHome = "$androidHome/ndk/$ndkVersion"
+    val toolchain = "$androidNdkHome/toolchains/llvm/prebuilt/$hostTag"
+    val target = "aarch64-linux-android"
+    val api = "33"
+    val cc = "$toolchain/bin/$target$api-clang"
+
+    environment("ANDROID_NDK_HOME", androidNdkHome)
+    environment("TOOLCHAIN", toolchain)
+    environment("TARGET", target)
+    environment("API", api)
+    environment("AR", "$toolchain/bin/llvm-ar")
+    environment("CC", cc)
+    environment("AS", cc)
+    environment("CXX", "$toolchain/bin/$target$api-clang++")
+    environment("LD", "$toolchain/bin/ld")
+    environment("RANLIB", "$toolchain/bin/llvm-ranlib")
+    environment("STRIP", "$toolchain/bin/llvm-strip")
+    environment("PATH", "$path:$androidHome/cmdline-tools/latest/bin")
+    environment("PATH", "$path:$toolchain/bin")
+    environment("CMAKE_TOOLCHAIN_FILE_aarch64_linux_android", "$androidNdkHome/build/cmake/android.toolchain.cmake")
+    environment("ANDROID_ABI", "arm64-v8a")
+    // Android arm64
+    commandLine("cargo", "build", "--release", "--target", "aarch64-linux-android", "-vvvv")
+    // Android arm
+    // commandLine("cargo", "build", "--release", "--target", "armv7-linux-androideabi")
+    // Android x86
+    // commandLine("cargo", "build", "--release", "--target", "i686-linux-android")
+    // Android x86_64
+    // commandLine("cargo", "build", "--release", "--target", "x86_64-linux-android")
+}
+
+tasks.register<Copy>("copyRustLibs") {
+    val buildType = System.getenv("BUILD_TYPE")
+    if (buildType == "MANUAL") {
+        // Copy the compiled library (.so file) to the appropriate JNI folder
+        from("$rustLibPath/target/aarch64-linux-android/release")
+        into("src/main/jniLibs/arm64-v8a")
+    } else {
+        // Download the .so file from a remote server
+        download.run {
+            src("https://github.com/madztheo/noir_android/releases/download/v0.30.0-1/libnoir_java.so")
+            dest("src/main/jniLibs/arm64-v8a")
+            overwrite(false)
+        }
+    }
+    include("lib${rustLibName}.so")
+}
+
+tasks.whenTaskAdded {
+    val buildType = System.getenv("BUILD_TYPE")
+    if (buildType == "MANUAL") {
+        if (name == "javaPreCompileDebug" || name == "javaPreCompileRelease") {
+            dependsOn("buildRust")
+        }
+    }
+    if (name.matches(Regex("merge.*JniLibFolders"))) {
+        dependsOn("copyRustLibs")
+    }
+}
---
+++ lib/src/main/java/com/noirandroid/lib/Noir.java
@@ -0,0 +1,18 @@
+package noir;
+
+import java.util.Map;
+
+public class Noir {
+    static {
+        System.loadLibrary("noir_java");
+    }
+
+    public static native Proof prove(String circuit_bytecode, Map<String, String> inital_witness, String proof_type, String num_points);
+
+    public static native boolean verify(String circuit_bytecode, Proof proof, String proof_type, String num_points);
+
+    public static native int setup_srs(String circuit_bytecode, String srs_path);
+    public static int setup_srs(String circuit_bytecode) {
+        return setup_srs(circuit_bytecode, null);
+    }
+}
